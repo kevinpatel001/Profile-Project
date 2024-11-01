f31de3ab2d135e08d704187015dbb320bf1369e5
Kevin Patel <patelkevin.4874@gmail.com>
2024-11-01 18:52:11 +0530

    Updated Portfolio
    Added auto slider the knowledge i know and little improvement on responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      animation:{
+        "loop-scroll":"loop-scroll 50s linear infinite"
+      },
+      keyframes:{
+        "loop-scroll":{
+          from:{ transform:"translateX(0)"},
+          to:{transform:"translateX(-100%)"},
+        },
+      },
       colors: {
        'dot-white': 'rgba(255, 255, 255, 0.2)', // Dot white with 20% opacity
         'dot-black': 'rgba(0, 0, 0, 0.2)',       // Dot black with 20% opacity
